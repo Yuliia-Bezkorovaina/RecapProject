51ed49dfabe78fbc7ce0e0560435506523e7df8a
yuliiabezkorovaina <yuliya.bezkorovayna@gmail.com>
2025-07-22 20:40:07 +0200

    init git

---
+++ src/test/java/org/example/recapproject/RecapProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.recapproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecapProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
